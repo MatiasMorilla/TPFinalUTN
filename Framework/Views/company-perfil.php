879f359deea3dc46d0fa0354ab4094f15029c5dc
Manu0912 <emanuelcacheda@hotmail.com>
2021-10-18 20:32:45 -0300

    styles in profile

--- Framework/Views/company-perfil.php
+++ Framework/Views/company-perfil.php
@@ -4,26 +4,22 @@
 ?>
 
 
-<div class="container">
+<div class="container mt-4">
 	<div class="row">
-		<div class="col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
-    	 <div class="well profile">
-            <div class="col-sm-12">
-                <div class="col-xs-12 col-sm-8">
-                    <h2><?php echo $companyName ?> </h2>
+           
+                            
+                <div class="col-6">
+                    <figure>
+                        <img src="../assets/Globant_.png" alt="" class="img-circle img-responsive" style="width:100%">
+                    </figure>
+                </div>   
+                <div class="col-6" style="box-shadow:5px 5px 5px 5px rgba(0,0,0,0.3); background-color:white;border-radius:10px">
+                    <h2 class="text-center mb-4 mt-3" style="text-transform:uppercase"><?php echo $companyName ?> </h2>
                     <p><strong>Email: </strong> <?php echo $companyEmail ?>. </p>
                     <p><strong>Numero de telefono: </strong> <?php echo $companyPhoneNumber ?> </p>
                     <p><strong>Direccion </strong> <?php echo $companyAddress ?> </p>
                     <p><strong>Cuil </strong> <?php echo $companyCuil ?> </p>
-                </div>             
-                <div class="col-xs-12 col-sm-4 text-center">
-                    <figure>
-                        <img src="../assets/Globant_.png" alt="" class="img-circle img-responsive">
-                    </figure>
-                </div>
-            </div>            
-    	 </div>                 
-		</div>
+                </div> 
 	</div>
 </div>
 
