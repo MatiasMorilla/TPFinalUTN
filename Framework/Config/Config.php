3fc628599a4c906863cfedfeb068924631eeaa7c
MatiMorilla <matiasmorilla@hotmail.com>
2021-11-01 11:44:41 -0300

    config database

--- Framework/Config/Config.php
+++ Framework/Config/Config.php
@@ -6,6 +6,12 @@ define("FRONT_ROOT", "/TPFinalUTN/Framework/");
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "TPFINAL");
+define("DB_USER", "root");
+define("DB_PASS", "1524");
+
 ?>
 
 
